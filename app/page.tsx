8cd51bd2de0f828b81b4a8f7d95086cb033ca02f
Seriy <tsbaranova@bk.ru>
2024-08-16 14:25:44 +0300

    nowTrending changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import { getBillboard } from '@/actions/get_billboard';
 import { getCollections } from '@/actions/get_collections';
 import { getDiscount } from '@/actions/get_discount';
 import { getNewArrivals } from '@/actions/get_new-arrivals';
-import { getNowTrending } from '@/actions/get_now_trending';
 import { getPopularTypes } from '@/actions/get_popular-types';
-import { getTopCategories } from '@/actions/get_top-categories';
+import { getPurchasedGoods } from '@/actions/get_purchased-goods';
 import Billboard from '@/components/home-page/billboard/billboard';
 import { Collection } from '@/components/home-page/collection/collection';
 import { Discount } from '@/components/home-page/discount/discount';
@@ -16,15 +15,13 @@ export default async function Home() {
   //запрос для получения billboard, на сервере при помощи fech(динамически)
   const billboardsPromise = getBillboard();
   //запрос для получения topCategories, на сервере при помощи fech(динамически)
-  const topCategoriesPromise = getTopCategories();
+  const purchasedGoodsPromise = getPurchasedGoods();
   //запрос для получения товаров, на сервере при помощи fech(динамически)
   const newArrivalsPromise = getNewArrivals();
   //запрос для получения коллекции товаров , на сервере при помощи fech(динамически)
   const collectionsPromise = getCollections();
   //запрос для получения популярных типов товаров , на сервере при помощи fech(динамически)
   const popularTypesPromise = getPopularTypes();
-  //запрос для получения товаров,которые сейчас в тренде, на сервере при помощи fech(динамически)
-  const nowTrendingPromise = getNowTrending();
   //запрос для получения товаров,которые сейчас в тренде, на сервере при помощи fech(динамически)
   const discountPromise = getDiscount();
   //запрос для получения отзывов о магазине, на сервере при помощи fech(динамически)
@@ -35,23 +32,24 @@ export default async function Home() {
     discount,
     newArrivals,
     popularTypes,
-    nowTrending,
+    purchasedGoods,
   ] = await Promise.all([
     billboardsPromise,
     collectionsPromise,
     discountPromise,
     newArrivalsPromise,
     popularTypesPromise,
-    nowTrendingPromise,
+    purchasedGoodsPromise,
   ]);
 
+  // console.log('purchasedGoods:', purchasedGoods);
   return (
     <main className="flex  flex-col ">
       <Billboard billboards={billboards} />
       <NewArrivals products={newArrivals.product} />
       <Collection collections={collections} />
       <PopularTypes popularTypes={popularTypes} />
-      <NowTrending nowTrending={nowTrending.product} />
+      <NowTrending nowTrending={purchasedGoods} />
       <Discount discount={discount.product} />
     </main>
   );
