81a1090159d196a5a2a496db38af7bb3d5a5fa41
Seriy <tsbaranova@bk.ru>
2024-08-17 15:46:15 +0300

    billbord changes

--- components/home-page/billboard/billboard.tsx
+++ components/home-page/billboard/billboard.tsx
@@ -13,7 +13,7 @@ import { SliderControls } from './slider-controls/slider-controls';
 import { Arrows } from './arrows/arrows';
 
 type BillboardPropsType = {
-  billboards: BillboardType[];
+  billboards: BillboardType[] | null;
 };
 
 const Billboard: FC<BillboardPropsType> = ({ billboards }) => {
@@ -40,45 +40,65 @@ const Billboard: FC<BillboardPropsType> = ({ billboards }) => {
   const scrollNext = useCallback(() => {
     api?.scrollNext();
   }, [api]);
-
+  // const ar = [] as [];
   return (
-    <div className=" relative">
-      <Carousel
-        setApi={setApi}
-        opts={{
-          align: 'start',
-          loop: true,
-          duration: 50,
-        }}
-      >
-        <CarouselContent>
-          {billboards.map((slide) => (
-            <CarouselItem key={slide.id}>
-              <Slide slide={slide} key={slide.id} />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
-      {/*  кастомный компонент - номер слайда */}
-      <div className="shared_container">
-        <div className=" relative mt-[-12%]">
-          <div className={styles.row}>
-            {api?.scrollSnapList().map((_, index) => {
-              return (
-                <SliderControls
-                  index={index}
-                  selectedIndex={selectedIndex}
-                  scrollTo={scrollTo}
-                  key={index}
-                />
-              );
-            })}
+    <>
+      {billboards ? (
+        billboards.length === 0 ? (
+          <div
+            className=" flex bg-slate-200 w-full h-[150px] sm:h-[250px] md:h-[350px]  justify-center 
+          items-center md:text-xl lg:h-[550px] lg:text-2xl"
+          >
+            The slider is empty
           </div>
+        ) : (
+          <div className=" relative">
+            <Carousel
+              setApi={setApi}
+              opts={{
+                align: 'start',
+                loop: true,
+                duration: 50,
+              }}
+            >
+              <CarouselContent>
+                {billboards.map((slide) => (
+                  <CarouselItem key={slide.id}>
+                    <Slide slide={slide} key={slide.id} />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
+            {/*  кастомный компонент - номер слайда */}
+            <div className="shared_container">
+              <div className=" absolute bottom-[19%] top-0 w-full">
+                <div className={styles.row}>
+                  {api?.scrollSnapList().map((_, index) => {
+                    return (
+                      <SliderControls
+                        index={index}
+                        selectedIndex={selectedIndex}
+                        scrollTo={scrollTo}
+                        key={index}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+            {/*  кастомный компонент - стрелки */}
+            <Arrows prevArrow={scrollPrev} nextArrow={scrollNext} />
+          </div>
+        )
+      ) : (
+        <div
+          className=" flex bg-slate-200 w-full h-[350px] md:h-[450px]  text-red-500 justify-center 
+        items-center md:text-xl lg:h-[550px] lg:text-2xl"
+        >
+          The slider is not loaded, something went wrong!
         </div>
-      </div>
-      {/*  кастомный компонент - стрелки */}
-      <Arrows prevArrow={scrollPrev} nextArrow={scrollNext} />
-    </div>
+      )}
+    </>
   );
 };
 
