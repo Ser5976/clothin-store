d2aedd950bd3969dbcf5c2fb481fcdfaab88bd66
Seriy <tsbaranova@bk.ru>
2025-06-01 10:11:26 +0300

    product create:rating

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -98,6 +98,7 @@ export async function POST(request: Request) {
           }),
         },
         discount: discount ?? null,
+        rating: { create: {} },
       },
     });
     return NextResponse.json({ message: 'Data is saved' });
